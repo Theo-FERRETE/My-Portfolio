5f831b66bb0cf58b1a2fbb0778382828f03bbd42
Theo-FERRETE <theo.ferrete@laplateforme.io>
2026-03-20 21:26:07 +0100

    changement auth

--- lib/auth/auth-config.ts
+++ lib/auth/auth-config.ts
@@ -1,8 +1,4 @@
-// Configuration d'authentification
-// Ce fichier contient le hash bcrypt car .env interprète les $ comme des variables
-
 export const AUTH_CONFIG = {
-  adminEmail: 'admin@portfolio.com',
-  // Hash bcrypt du mot de passe admin (à changer en production)
-  adminPasswordHash: '$2b$10$NPcT4ANVo/0hXH4Q32pdVut1KEr3AR2VuOcU1x8kLacKDrYhqx8wu',
+  adminEmail: process.env.ADMIN_EMAIL || 'admin@portfolio.com',
+  adminPasswordHash: process.env.ADMIN_PASSWORD_HASH || '$2b$10$NPcT4ANVo/0hXH4Q32pdVut1KEr3AR2VuOcU1x8kLacKDrYhqx8wu',
 } as const;
